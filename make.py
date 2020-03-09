c4adc0c698fa5be749cfb3486af3a5e8f0db931a
MentalBlood <necheporenko.stepan@yandex.ru>
2020-03-09 15:31:12 +0300

    added building script

---
+++ make.py
@@ -0,0 +1,15 @@
+import re
+print(re.sub('//.+', '', '//# sourceMappingURL=dat.gui.js.map\nlol'))
+
+def includeJSFile(textToIncludeIn, fileToIncludeName):
+    with open(fileToIncludeName, 'r') as fileToInclude:
+        return textToIncludeIn.replace(' src="' + fileToIncludeName + '">', '>' + fileToInclude.read())
+
+with open('game.html', 'r') as inputFile, open('gameBuild.html', 'w') as outputFile:
+    inputFileText = inputFile.read()
+    outputFileText = includeJSFile(inputFileText, 'main.js')
+    outputFileText = includeJSFile(outputFileText, 'dat.gui.js')
+
+    outputFileText = re.sub(' +', ' ', outputFileText)
+    outputFileText = re.sub('//*\n', '', outputFileText)
+    outputFile.write(outputFileText)
